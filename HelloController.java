4987a8859bb26746c7391c0a75e1f36714dfbdd7
rajumandala <64860940+rajumandala@users.noreply.github.com>
2022-12-16 14:56:03 +0530

    Create HelloController.java

---
+++ HelloController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello, " + name + "!";
+    }
+}
